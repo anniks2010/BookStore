6790430eb4a19346a090f76d3b4842437e071e19
anniks2010 <anulember@hot.ee>
2021-01-16 22:05:35 +0200

    Second lesson

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,24 @@ const path =require('path');
 const rootDirectory=require('../utilities/path');
 const express=require('express');
 const router =express.Router();
+const products =[];
 ///mini app pluggable to another express app
 
 router.get('/add-product',(req,res)=>{
+    res.render('add-product.ejs',{
+        pageTitle: 'Add Product',
+        path: '/admin/add-product'
+    });
    ///res.sendFile(path.join(__dirname, '..', 'views','add-product.html'));
-   res.sendFile(path.join(rootDirectory,'views','add-product.html'));
+   ///res.sendFile(path.join(rootDirectory,'views','add-product.html'));
 });
 
 router.post('/add-product', (req,res)=>{
+    products.push({title: req.body.title});
     res.redirect('/'); ///koduleht on shop.html leht
 });
 
-module.exports=router;
+///module.exports=router;
+exports.router=router;
+exports.products=products;
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,18 @@
 const path =require('path');
 const rootDirectory=require('../utilities/path');
 const express=require('express');
+const adminData=require('./admin');
 const router =express.Router();
 
 router.get('/',(req,res)=>{
-   res.sendFile(path.join(rootDirectory,'views','shop.html'));
+   const products =adminData.products;
+   res.render('shop.ejs',{
+      productsMain: products,
+      pageTitle: 'Main Page',
+      path: '/'
+
+   });
+   ///res.sendFile(path.join(rootDirectory,'views','shop.html'));
 });
 
 
